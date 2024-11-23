38014dc66c23ba74501f4ee3e1b51d722d71e667
Vikas Singh <shyvikas63@gmail.com>
2024-11-23 23:11:18 +0530

    git init

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const { getCpuUsage, simulateHeavyTask } = require('./tasks');
+const client = require('prom-client');
+const { register } = client;
+const app = express();
+const port = 3000;
+
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register });
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Endpoint to get CPU usage
+app.get('/cpu-usage', getCpuUsage);
+
+// Simulate a heavy task
+app.get('/heavy', simulateHeavyTask);
+
+// metrics
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  const metrics = await register.metrics();
+  res.send(metrics);
+});
+
+// Simple Hello World endpoint
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ tasks.js
@@ -0,0 +1,41 @@
+
+const osUtils = require('os-utils');
+const faker = require('faker');
+
+function getCpuUsage(req, res) {
+  osUtils.cpuUsage((usage) => {
+    res.json({ usage: (usage * 100).toFixed(2) });
+  });
+}
+
+function simulateHeavyTask(req, res) {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  const startTime = Date.now();
+  let progress = 0;
+
+  const interval = setInterval(() => {
+    progress += faker.datatype.number({ min: 1, max: 20 }); // Random progress increment between 1 and 20
+    if (progress > 100) progress = 100; // Ensure progress does not exceed 100
+    const elapsedTime = Date.now() - startTime;
+
+    osUtils.cpuUsage((usage) => {
+      if (progress < 100 && Math.random() < 0.1) { // 10% chance of error
+        clearInterval(interval);
+        res.write(`data: Error: Something went wrong\n\n`);
+        res.end();
+      } else {
+        res.write(`data: ${progress}% - Elapsed time: ${elapsedTime}ms - CPU Usage: ${(usage * 100).toFixed(2)}%\n\n`);
+        if (progress === 100) {
+          clearInterval(interval);
+          res.write(`data: Success: Task completed\n\n`);
+          res.end();
+        }
+      }
+    });
+  }, faker.datatype.number({ min: 500, max: 1500 })); // Random interval between 500ms and 1500ms
+}
+
+module.exports = { getCpuUsage, simulateHeavyTask };
