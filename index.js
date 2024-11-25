f039e3f9771c467e5f1ffd864ef7c58888a65a34
Vikas Singh <shyvikas63@gmail.com>
2024-11-25 20:07:13 +0530

    index.js updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 const { register, collectDefaultMetrics } = client;
 collectDefaultMetrics({ register });
